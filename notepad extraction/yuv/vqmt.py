97bf489d02e5be4778970f8e2d9da2f9db36ff7d
Priyansh <priyanshzalavadiya007@gmail.com>
2019-02-21 00:14:12 +0530

    file

--- notepad extraction/yuv/vqmt.py
+++ notepad extraction/yuv/vqmt.py
@@ -7,6 +7,18 @@ Created on Thu Feb  7 14:04:12 2019
 
 
 import os
-os.system('ffmpeg -i Dog.mp4 -c:v rawvideo -pix_fmt yuv420p out.yuv')
-os.system('VQMT.exe out.yuv out.yuv 360 640 540 1 results PSNR SSIM VIFP')
-    
+#os.system('ffmpeg -i Dog.mp4 -c:v rawvideo -pix_fmt yuv420p out.yuv')
+#os.system('VQMT.exe out.yuv out.yuv 360 640 540 1 results PSNR SSIM VIFP')
+
+import pandas
+df = pandas.read_csv('results_psnr.csv', index_col='frame')
+avgpsnr=df[['value']].mean()
+avgpsnr=avgpsnr.value
+
+df = pandas.read_csv('results_ssim.csv', index_col='frame')
+avgssim=df[['value']].mean()
+avgssim=avgssim.value
+
+df = pandas.read_csv('results_vifp.csv', index_col='frame')
+avgvifp=df[['value']].mean()
+avgvifp=avgvifp.value
