570bd559a00419792baa5073cde8cac42afc5de5
JAY DUDHAT <jaydudhatnk@gmail.com>
2019-02-20 15:17:22 +0530

    Update CreateVideoDetails.py

--- Compression/CreateVideoDetails.py
+++ Compression/CreateVideoDetails.py
@@ -1,4 +1,7 @@
-import os
-os.chdir("C:\Program Files\mediainfo")
-os.getcwd()
-log = os.system('"MediaInfo.exe" first.MOV >D:\log.txt')
+def CreateVideoDetail(VideoPath):
+    import os
+    from pathlib import Path, PureWindowsPath
+    VideoPath = str(PureWindowsPath(Path("C:/Forest_Slice.mov")))
+    os.chdir("C:\Program Files\mediainfo")
+    os.getcwd()
+    os.system('"MediaInfo.exe" '+VideoPath+' >D:\log.txt')
