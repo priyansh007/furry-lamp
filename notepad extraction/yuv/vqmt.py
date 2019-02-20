6eda5754f81db68b2ce035b1b8cc5b88f75caaf0
Priyansh <priyanshzalavadiya007@gmail.com>
2019-02-20 23:56:09 +0530

    chnge

--- notepad extraction/yuv/vqmt.py
+++ notepad extraction/yuv/vqmt.py
@@ -5,9 +5,8 @@ Created on Thu Feb  7 14:04:12 2019
 @author: PRIYANSHZALAVADIYA
 """
 
-from os import listdir
-from os.path import isfile, join
-mypath = "F:/project/compression details/"
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
+import os
+os.system('ffmpeg -i Dog.mp4 -c:v rawvideo -pix_fmt yuv420p out.yuv')
+os.system('VQMT.exe out.yuv out.yuv 360 640 540 1 results PSNR SSIM VIFP')
     
