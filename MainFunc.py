05990b48e197769ae3422b32338626aca07623a7
Alkesh Vaghela <alkeshgamer@gmail.com>
2019-02-22 15:41:42 +0530

    Seperate width and height instead of dimensions in dataframe

--- MainFunc.py
+++ MainFunc.py
@@ -9,10 +9,11 @@ corePath = str(PureWindowsPath(Path("E:/New folder")))
 ffmpeg = '"' + str(PureWindowsPath(Path("E:/N.I.B.B.A.S/StaxRip 1.9.0.0/Apps/ffmpeg/ffmpeg.exe"))) + '"'
 mkvmerge = '"' + str(PureWindowsPath(Path("E:/N.I.B.B.A.S/StaxRip 1.9.0.0/Apps/MKVToolNix/mkvmerge.exe"))) + '"'
 mediaInfo = str(PureWindowsPath(Path("E:/New folder/MediaInfo/")))
+vqmt = '"' + str(PureWindowsPath(Path("E:/VQMT/VQMT.exe"))) + '"'
 videoFileList = [f for f in listdir(corePath + "\\Input\\") if isfile(join(corePath + "\\Input\\", f))]
 presets = ['superfast', 'veryfast', 'faster', 'fast', 'medium', 'slow', 'slower']
 presets1 = ['superfast', 'veryfast']
-df_for_current_video = pd.DataFrame(columns=['Video Name',' Dimensions', 'Video Length', 'Frames per Second',
+df_for_current_video = pd.DataFrame(columns=['Video Name',' Width', 'Height', 'Video Length', 'Frames per Second',
                 'Frame Count', 'Original Bitrate', 'Original Size', 'Scene Count', 'Avg Motion %',
                 'Avg PCC', 'Compression Preset','Compression Duration', 'Compressed Bitrate', 'Compressed Size'])
 for video in videoFileList:
