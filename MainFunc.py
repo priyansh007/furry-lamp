336e567a6631bdf1d00fb1a7ec5ac76308a03831
Alkesh Vaghela <alkeshgamer@gmail.com>
2019-02-26 22:48:33 +0530

    Updated time tracking code and minor changes

--- MainFunc.py
+++ MainFunc.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import time
-import datetime
 from os import listdir
 from os.path import isfile, join
 from Compression import compressFunc,CreateVideoDetails,vqmt
@@ -8,36 +7,38 @@ from FeatureExtraction import Feature_Extraction
 from pathlib import Path, PureWindowsPath
 
 corePath = str(PureWindowsPath(Path("E:/New folder")))
-ffmpeg = '"' + str(PureWindowsPath(Path("E:/N.I.B.B.A.S/StaxRip 1.9.0.0/Apps/ffmpeg/ffmpeg.exe"))) + '"'
-mkvmerge = '"' + str(PureWindowsPath(Path("E:/N.I.B.B.A.S/StaxRip 1.9.0.0/Apps/MKVToolNix/mkvmerge.exe"))) + '"'
-mediaInfo = str(PureWindowsPath(Path("E:/New folder/MediaInfo/")))
-VQMT = '"' + str(PureWindowsPath(Path("E:/VQMT/VQMT.exe"))) + '"'
+ffmpeg = '"E:\\N.I.B.B.A.S\\StaxRip 1.9.0.0\\Apps\\ffmpeg\\ffmpeg.exe"'
+mkvmerge = '"E:\\N.I.B.B.A.S\\StaxRip 1.9.0.0\\Apps\\MKVToolNix\\mkvmerge.exe"'
+mediaInfo = "E:\\New folder\\MediaInfo\\"
+VQMT = '"E:\\VQMT\\VQMT.exe"'
 videoFileList = [f for f in listdir(corePath + "\\Input\\") if isfile(join(corePath + "\\Input\\", f))]
 presets = ['superfast', 'veryfast', 'faster', 'fast', 'medium', 'slow', 'slower']
-presets1 = ['superfast', 'veryfast']
+presets1 = ['superfast', 'veryfast', 'medium']
 df_for_video_data = pd.DataFrame(columns=['Video Name','Width', 'Height', 'Video Length', 'Frames per Second',
                 'Frame Count', 'Original Bitrate', 'Original Size', 'Scene Count', 'Avg Motion %',
                 'Avg PCC', 'Avg Intensity', 'Compression Preset','Compression Duration', 'Compressed Bitrate', 'Compressed Size'])
+print('Obtained videos are : ',videoFileList)
 for video in videoFileList:
     print("Processing file : " + video)
     start = time.time()
     Feature_Extraction.feature_extr(corePath + "\\Input\\" + video, df_for_video_data, presets)
     end = time.time()
-    feTime = str(datetime.timedelta(end - start))
-    print('Feature Extraction took ' + feTime + ' seconds')
+    feTime = end - start
+    print('Feature Extraction took ' + time.strftime("%H:%M:%S", time.gmtime(feTime)))
     
     start = time.time()
-    compressFunc.compressdis(corePath, video, presets1, ffmpeg, mkvmerge)
+    compressFunc.compressdis(corePath, video, presets, ffmpeg, mkvmerge)
     width, height = CreateVideoDetails.CreateVideoDetail(corePath, video, mediaInfo, df_for_video_data)
     CreateVideoDetails.CreateCompVideoDetail(corePath, video, mediaInfo, df_for_video_data)
     frames = CreateVideoDetails.retrieveCompressionDetail(corePath, video, df_for_video_data)
     end = time.time()
-    clTime = str(datetime.timedelta(end - start))
-    print('Compression and Logging took ' + clTime + ' seconds')
+    clTime = end - start
+    print('Compression and Logging took ' + time.strftime("%H:%M:%S", time.gmtime(clTime)))
     
     start = time.time()
-    vqmt.videoQualityMeasure(corePath, video, presets1, width, height, frames, ffmpeg, VQMT)
+    vqmt.videoQualityMeasure(corePath, video, presets, width, height, frames, ffmpeg, VQMT)
     end = time.time()
-    vqmtTime = str(datetime.timedelta(end - start))
-    print('Video Quality Measurement took ' + vqmtTime + ' seconds')
-df_for_video_data.to_csv("E:\\New folder\\Dataset\\Dataset.csv")
+    vqmtTime = end - start
+    print('Video Quality Measurement took ' + time.strftime("%H:%M:%S", time.gmtime(vqmtTime)))
+    totalProcessTime = float(feTime) + float(clTime) + (vqmtTime)
+    print('Processing complete\nTotal Processing Time : ' + time.strftime("%H:%M:%S", time.gmtime(totalProcessTime)))
