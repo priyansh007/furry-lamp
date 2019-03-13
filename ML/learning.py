a3a9c0a9c6cc69c40a4ce734b32238ad60c79c45
Priyansh <priyanshzalavadiya007@gmail.com>
2019-03-13 17:26:28 +0530

    ml

---
+++ ML/learning.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  3 12:49:05 2018
+
+@author: PRIYANSHZALAVADIYA
+"""
+
+import numpy as np
+
+import pandas
+df = pandas.read_csv('data_final_with_output_presets.csv')
+print()
+#Input array
+X=np.array(df[['Avg.PCC','Avg.Motion','Scene.Count','Video.Length','Frames.per.Second','Original.Bitrate']])
+
+#Output
+y=np.array(df[['Compression.Preset']])
+
+#Sigmoid Function
+def sigmoid (x):
+    return 1/(1 + np.exp(-x))
+
+#Derivative of Sigmoid Function
+def derivatives_sigmoid(x):
+    return x * (1 - x)
+
+#Variable initialization
+epoch=5000 #Setting training iterations
+lr=0.1 #Setting learning rate
+inputlayer_neurons = X.shape[1] #number of features in data set
+hiddenlayer_neurons = 50 #number of hidden layers neurons
+output_neurons = 1 #number of neurons at output layer
+
+#weight and bias initialization
+wh=np.random.uniform(size=(inputlayer_neurons,hiddenlayer_neurons))
+bh=np.random.uniform(size=(1,hiddenlayer_neurons))
+wout=np.random.uniform(size=(hiddenlayer_neurons,output_neurons))
+bout=np.random.uniform(size=(1,output_neurons))
+
+for i in range(epoch):
+    print('epoch ',i)
+#Forward Propogation
+    hidden_layer_input1=np.dot(X,wh)
+    hidden_layer_input=hidden_layer_input1 + bh
+    hiddenlayer_activations = sigmoid(hidden_layer_input)
+    output_layer_input1=np.dot(hiddenlayer_activations,wout)
+    output_layer_input= output_layer_input1+ bout
+    output = sigmoid(output_layer_input)
+
+#Backpropagation
+    E = y-output
+    slope_output_layer = derivatives_sigmoid(output)
+    slope_hidden_layer = derivatives_sigmoid(hiddenlayer_activations)
+    d_output = E * slope_output_layer
+    Error_at_hidden_layer = d_output.dot(wout.T)
+    d_hiddenlayer = Error_at_hidden_layer * slope_hidden_layer
+    wout += hiddenlayer_activations.T.dot(d_output) *lr
+    bout += np.sum(d_output, axis=0,keepdims=True) *lr
+    wh += X.T.dot(d_hiddenlayer) *lr
+    bh += np.sum(d_hiddenlayer, axis=0,keepdims=True) *lr
+    
+print(output)
